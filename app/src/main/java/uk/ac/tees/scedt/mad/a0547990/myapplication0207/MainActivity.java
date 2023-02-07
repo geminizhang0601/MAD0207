223295af073fc2e72cabc68b5ad356661a673ed5
Yuetong ZHANG <a0547990@linux5.scedt.tees.ac.uk>
2023-02-07 16:22:41 +0000

    try coomit

---
+++ app/src/main/java/uk/ac/tees/scedt/mad/a0547990/myapplication0207/MainActivity.java
@@ -0,0 +1,14 @@
+package uk.ac.tees.scedt.mad.a0547990.myapplication0207;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
